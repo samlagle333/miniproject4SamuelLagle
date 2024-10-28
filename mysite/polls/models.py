aa64fd327e581726f120aaddcec0ebaecd026748
samlagle333 <splagle@mail.fhsu.edu>
2024-10-28 13:45:54 -0500

    Built out an additional feature to show published dates for questions and choices in the admin feature

--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib import admin
 from django.utils import timezone
 import datetime
 
@@ -10,8 +11,14 @@ class Question(models.Model):
     def __str__(self):
         return self.question_text
 
+    @admin.display(
+        boolean=True,
+        ordering="pub_date",
+        description="Published recently?",
+    )
     def was_published_recently(self):
-        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+        now = timezone.now()
+        return now - datetime.timedelta(days=1) <= self.pub_date <= now
 
 
 
